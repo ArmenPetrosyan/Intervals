e0006aabf6d48f01e13967d3adf3b8adf57c969f
Arnie <arnie@templatemonster.me>
2015-09-14 17:27:31 +0300

    UPD: creation of new Interval GUI items

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,16 @@
 (function($){
+    var $addBtn = $('.addButton'),
+        $gallery = $('.interval-gallery'),
+        $minInput = $('.minInput'),
+        $maxInput = $('.maxInput');
 
+    $addBtn.on('click',function(e){
+        e.preventDefault();
+
+        var min = $minInput.val(),
+            max = $maxInput.val(),
+            $li = $('<li>',{'class':'gallery-item','data-min': min, 'data-max': max});
+
+        $li.prependTo($gallery);
+    });
 })(jQuery);
